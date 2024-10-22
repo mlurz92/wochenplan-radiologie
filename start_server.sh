8a8fdb6f4816685cb4d85b35475a699605b711ec
mlurz92 <72194266+mlurz92@users.noreply.github.com>
2024-10-22 06:46:09 +0200

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-cd /srv/wochenplan-radiologie
-pm2 start server.js --name wochenplan-radiologie --env production
+
+# Ins Arbeitsverzeichnis wechseln
+cd /srv/wochenplan-radiologie || exit
+
+# Berechtigungen prüfen und setzen
+if [ "$(stat -c '%U' .)" != "mlurz92" ]; then
+    sudo chown -R mlurz92:mlurz92 .
+fi
+
+# PM2-Prozess starten
+pm2 start server.js --name wochenplan-radiologie -- --env production
